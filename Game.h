a1cecc8e0a360569534f74d11db67ffd504df998
tomas-jeong543 <wjdaudgns03@naver.com>
2025-05-25 23:25:21 +0900

    Update Game.h

--- Game.h
+++ Game.h
@@ -8,26 +8,35 @@
 #include <Windows.h>
 
 class Game {
-    private:
-        int level;
-        int score;
-        int lines;
-        int i;
-	    int is_gameover=0;
-        char keytemp;
-        char total_block[21][14];
-        Stages* stages;
-        Printer* printer;
-        Block* curr_block;
-        Block* next_block;
-    public:
-        Game();
-        void run();
-        int init();
-        int input_data();
-        int move_block();
-        int strike_check();
-        int merge_block();
-        int check_full_line();
-        int rotate();
+private:
+    int level;
+    int score;
+    int lines;
+    int i;
+    int is_gameover = 0;
+    char keytemp;
+    char total_block[21][14];
+    Stages* stages;
+    Printer* printer;
+    Block* curr_block;
+    Block* next_block;
+public:
+    Game();
+    void run();
+    int init();
+    //거꾸로 나라 초기화하는 함수
+    int init_Reversed();
+    int input_data();
+    int move_block();
+    //거꾸로 나라의 블록 움직임을 나타내는 추가된 함수
+    int move_block_Reversed();
+    int strike_check();
+    //거꾸로 나라 전용 충돌 감지 함수 거의 strike_check함수와 비슷하지만 추가된 내용 블록의 y좌표가 0보다 작아도 충돌로 가정한다는 내용이 추가되었습니다
+    int strike_check_Reversed();
+    int merge_block();
+    int check_full_line();
+    int rotate();
+    //거꾸로 나라인지 여부를 판단하는 함수로 신규 추가됨
+    bool isReverseWorld(int level);
+   
 };
