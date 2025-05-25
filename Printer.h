a818c08b382cb9bc5736d4263f53ac7b167567a7
tomas-jeong543 <wjdaudgns03@naver.com>
2025-05-25 23:26:30 +0900

    Update Printer.h

--- Printer.h
+++ Printer.h
@@ -8,22 +8,23 @@
 #include <Windows.h>
 
 class Printer {
-    private:
-        int ab_x;
-        int ab_y;
-    public:
-        Printer();
-        int get_x();
-        int get_y();
-        int gotoxy(int x, int y);
-        void SetColor(int color);
-        void show_keys();
-        int show_cur_block(Block& block);
-        int erase_cur_block(Block& block);
-        int show_next_block(Block& block, int level);
-        int show_logo();
-        int show_total_block(char total_block[21][14], int level);
-        int show_gamestat(int level, int score, int lines_left);
-        int show_clear_screen(int score);
-        int show_gameover();
+private:
+    int ab_x;
+    int ab_y;
+public:
+    Printer();
+    int get_x();
+    int get_y();
+    int gotoxy(int x, int y);
+    void SetColor(int color);
+    void show_keys();
+    int show_cur_block(Block& block);
+    int erase_cur_block(Block& block );
+    int show_next_block(Block& block, int level);
+    int show_logo();
+    //일부 내용이 변경된 함수입니다 cpp파일에 설명이 적혀 있습니다
+    int show_total_block(char total_block[21][14], int level);
+    int show_gamestat(int level, int score, int lines_left);
+    int show_clear_screen(int score);
+    int show_gameover();
 };
